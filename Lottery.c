2a6eedd860989b54859c872399abf22ba1e07772
holicguard <risliisexy@hotmail.com>
2018-03-27 13:14:29 +0700

    Edit Lottery.c

--- Lottery.c
+++ Lottery.c
@@ -2,12 +2,12 @@
 void main()
 {
   int WinLotteryNo[10] = {1525, 1711, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
-  float WinAmount[10] = {888, 999, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
+  float WinAmount[10] = {777, 888, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
   int N = 10;
   int i, won; 
   
   int YourLotteryNo;  
-   gi
+   
   printf("Enter your Lottery Number (0 to end): "); 
   scanf("%d", &YourLotteryNo); 
   while (YourLotteryNo != 0) 
